00d6ee306a03592ed3ae0eb4d27651bdf2bd7f00
André Maldonado <andre.maldonado@gmail.com>
2015-04-01 13:57:03 -0300

    Listing the books on main activity

--- app/src/main/java/br/com/jcnaveia/biblia/BooksActivity.java
+++ app/src/main/java/br/com/jcnaveia/biblia/BooksActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.GridView;
-
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class BooksActivity extends ActionBarActivity {
 
@@ -13,10 +13,83 @@ public class BooksActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+        setTitle("Bíblia"); //TODO: Check a better way to set the title
 
-        GridView gridViewBooks = (GridView) findViewById(R.id.gridViewBooks);
-        //TODO: Set adapter for book list
-        //gridViewBooks.setAdapter();
+        ListView listViewBooks = (ListView) findViewById(R.id.listViewBooks);
+        listViewBooks.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getBooks()));
+    }
+
+    //TODO: Move this method to a better place
+    private String[] getBooks() {
+        return new String[]
+                    {
+                            "Gênesis",
+                            "Êxodo",
+                            "Levítico",
+                            "Números",
+                            "Deuteronômio",
+                            "Josué",
+                            "Juízes",
+                            "Rute",
+                            "1 Samuel",
+                            "2 Samuel",
+                            "1 Reis",
+                            "2 Reis",
+                            "1 Crônicas",
+                            "2 Crônicas",
+                            "Esdras",
+                            "Neemias",
+                            "Ester",
+                            "Jó",
+                            "Salmos",
+                            "Provérbios",
+                            "Eclesiastes",
+                            "Cânticos",
+                            "Isaías",
+                            "Jeremias",
+                            "Lamentações",
+                            "Ezequiel",
+                            "Daniel",
+                            "Oséias",
+                            "Joel",
+                            "Amós",
+                            "Obadias",
+                            "Jonas",
+                            "Miquéias",
+                            "Naum",
+                            "Habacuque",
+                            "Sofonias",
+                            "Ageu",
+                            "Zacarias",
+                            "Malaquias",
+                            "Mateus",
+                            "Marcos",
+                            "Lucas",
+                            "João",
+                            "Atos",
+                            "Romanos",
+                            "1 Coríntios",
+                            "2 Coríntios",
+                            "Gálatas",
+                            "Efésios",
+                            "Filipenses",
+                            "Colossenses",
+                            "1 Tessalonicenses",
+                            "2 Tessalonicenses",
+                            "1 Timóteo",
+                            "2 Timóteo",
+                            "Tito",
+                            "Filemom",
+                            "Hebreus",
+                            "Tiago",
+                            "1 Pedro",
+                            "2 Pedro",
+                            "1 João",
+                            "2 João",
+                            "3 João",
+                            "Judas",
+                            "Apocalipse"
+                    };
     }
 
 
